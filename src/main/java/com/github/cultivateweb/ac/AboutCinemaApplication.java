415809e6303622b844a9b75782167aafffc59abb
cultivateweb <cultivateweb@gmail.com>
2025-04-10 20:01:57 +0300

    feat: replace bootstrap script with generated Spring Boot project

---
+++ src/main/java/com/github/cultivateweb/ac/AboutCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.github.cultivateweb.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cultivateweb/ac/AboutCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cultivateweb.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
